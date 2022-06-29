08b2caf63f658c63e3be55f7eef3aaea920b15e5
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-06-29 14:49:02 +0400

    Get Special Hazir

--- js/js2/getSpecialOffer.js
+++ js/js2/getSpecialOffer.js
@@ -10,3 +10,22 @@ function plus() {
   quantity++;
   document.querySelector(".getSpecialQuantity").placeholder = quantity;
 }
+
+// date input js code
+$(function () {
+  $("#datepicker").datepicker({
+    dateFormat: "dd-mm-yy",
+    duration: "fast",
+  });
+});
+
+// radio input change menu
+function menuChange() {
+  if (document.querySelector("#r3").checked) {
+    document.querySelector(".specialOfferAir").classList.remove("content");
+    document.querySelector(".specialOffer").classList.add("content");
+  } else {
+    document.querySelector(".specialOfferAir").classList.add("content");
+    document.querySelector(".specialOffer").classList.remove("content");
+  }
+}
