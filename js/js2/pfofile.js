676e77c3d393b8da6a49eb5ece8091d9ce301e6a
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-06-30 09:49:36 +0400

    Profil Hazir

--- js/js2/pfofile.js
+++ js/js2/pfofile.js
@@ -1,6 +1,6 @@
 function view(e) {
   let line = document.querySelector(".downLine");
-  let arr = ["#d1", "#d2", "#d3", "#d4", "#d5"];
+  let arr = [".d1", ".d2", ".d3", ".d4", ".d5"];
   console.log(line.style.top);
   //   console.log(typeof parseInt(e.target.id[4]));
   let viewMenu = document.getElementById(`${e.target.id[4]}`);
@@ -13,12 +13,12 @@ function view(e) {
     // console.log(elements[i]);
     if (i + 1 !== parseInt(e.target.id[4])) {
       elements[i].classList.add("displayNone");
-      // document.querySelector(`${arr[i]}`).style.display = "none";
-      console.log(document.querySelector(${arr[i]}));
+      document.querySelector(`${arr[i]}`).style.display = "none";
+
+      // console.log(document.querySelector(${arr[i]}));
     } else {
       elements[i].classList.remove("displayNone");
       document.querySelector(`${arr[i]}`).style.display = "block";
-      console.log(document.querySelector(`${arr[i]}`));
     }
   }
 }
